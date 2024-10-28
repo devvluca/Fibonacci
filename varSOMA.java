bfe481b0bc3fc173666f7e61b02bd9a4a053bd97
Luca Aguiar <133829719+devvluca@users.noreply.github.com>
2024-10-28 18:03:42 -0300

    Create varSOMA.java

---
+++ varSOMA.java
@@ -0,0 +1,9 @@
+int INDICE = 12, SOMA = 0, K = 1;
+enquanto K < INDICE faça {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+// Ao final do processamento:
+// O valor de SOMA será a soma dos números de 2 a 12, ou seja: 2 + 3 + 4 + ... + 12.
+// O resultado final será SOMA = 78.
